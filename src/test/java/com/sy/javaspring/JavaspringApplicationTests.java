3d035e01d1da44972613440f1f871b3165d3c53b
yiyeqianqiu <1367381358@qq.com>
2020-08-10 14:45:58 +0800

    第一个接口

---
+++ src/test/java/com/sy/javaspring/JavaspringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sy.javaspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaspringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
